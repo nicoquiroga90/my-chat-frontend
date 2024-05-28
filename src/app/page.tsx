28f73e41728ec739c3e863924c6bc3b96a82420a
Nico <akebra77@gmail.com>
2024-05-28 17:59:47 +0200

    changed fetch URL

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -39,7 +39,7 @@ export default function Home() {
   const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
 
-    await fetch("http://localhost:8000/api/messages", {
+    await fetch("https://my-chat-backend-sable.vercel.app/api/messages", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({
